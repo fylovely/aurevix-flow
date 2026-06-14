eb256ec2ef2114e4f25e8be0832a9776994c842d
fylovely <rafikayadaa@gmail.com>
2026-06-14 21:18:48 +0800

    update app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.aurevix.flow',
   appName: 'AurevixFlow',
   webDir: 'dist',
   plugins: {
